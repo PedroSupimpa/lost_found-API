c6dc2f17b887203a2eec9122b16520d2e399dd49
Pedro Souza <shiro2303@gmail.com>
2023-10-13 22:25:01 -0300

    adding route to get image

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import AppDataSource from './data-source';
 import routes from './routes';
+import path from 'path';
+import fs from 'fs';
 require('dotenv').config();
-
 
 
 AppDataSource.initialize().then(() => {
@@ -12,6 +13,17 @@ AppDataSource.initialize().then(() => {
 
     app.use(routes)
 
+    app.get('/user/images/:imageFileName', (req, res) => {
+        const imageFileName = req.params.imageFileName;
+        const imagePath = path.join(__dirname, '/images', imageFileName);
+
+
+        if (fs.existsSync(imagePath)) {
+            res.sendFile(imagePath);
+        } else {
+            res.status(404).send('Imagem não encontrada');
+        }
+    });
     return app.listen(process.env.PORT)
 
 })
