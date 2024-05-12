0ceab8319490e477633998ccc966b61d403b9ccb
Pedro Souza <shiro2303@gmail.com>
2024-05-12 18:56:10 -0300

    Update PostService to include imageLink and postId in images array

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -9,6 +9,7 @@ import { imageUpload } from "../utils/imageUpload";
 import { Request, Response } from "express";
 import fs from "fs";
 import { ILike } from "typeorm";
+import { Post } from "../entities/Post";
 
 
 
@@ -115,15 +116,15 @@ export class PostService {
             const postQty = params.postQty ? parseInt(params.postQty.toString(), 10) : 10;
             const sortPost = params.sortPost ? params.sortPost : 'createdDate';
 
-            let posts;
-            let totalPosts;
+            let posts : Post[];
+            let totalPosts: number;
 
 
             if (params.latitude && params.longitude && params.locationRange) {
-
                 const locationQuery = `circle(point(:longitude, :latitude), :radius) @> location`;
-
+        
                 posts = await postRepository.createQueryBuilder("post")
+                    .leftJoinAndSelect("post.category", "category")
                     .where(query)
                     .andWhere(locationQuery, {
                         longitude: params.longitude,
@@ -134,7 +135,7 @@ export class PostService {
                     .take(postQty)
                     .orderBy(`post.${sortPost}`, 'DESC')
                     .getMany();
-
+        
                 totalPosts = await postRepository.createQueryBuilder("post")
                     .where(query)
                     .andWhere(locationQuery, {
@@ -143,21 +144,30 @@ export class PostService {
                         radius: params.locationRange
                     })
                     .getCount();
-                    
             } else {
                 posts = await postRepository.find({
+                    relations: ["category"], 
                     where: query,
                     skip: (page - 1) * postQty,
                     take: postQty,
                     order: { [sortPost]: 'DESC' }
                 });
-
+        
                 totalPosts = await postRepository.count({ where: query });
             }
-
+        
+            console.log(posts);
+            
             const totalPages = Math.ceil(totalPosts / postQty);
             
             await Promise.all(posts.map(async post => {
+                if (post.category) { 
+                    const category = await postCategoryRepository.findOne({ where: { id: post.category.id } });
+                    if (category) {
+                        post.category = category;
+                    }
+                }
+        
                 const postImages = await postImageRepository.find({ where: { postId: post.id } });
                 post.images = postImages.map(postImage => ({
                     imageLink: `http://localhost:3000/user/images/${postImage.imageLink}.jpg`,
@@ -165,7 +175,7 @@ export class PostService {
                 }));
                 return post;
             }));
-
+        
             return { posts, totalPages };
         } catch (error) {
             throw error;
